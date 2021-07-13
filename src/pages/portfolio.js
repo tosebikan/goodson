ba374e4e9bd93684f14892be83a75964967762d2
tosebikan <tosebikan@gmail.com>
2021-07-13 00:51:28 +0000

    fix get unique values

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,25 +7,32 @@ function Portfolio({ location }) {
   const [images, setImages] = useState([]);
 
   const data = location.state;
+  console.log({ data });
 
   useEffect(() => {
     updateImages();
-  }, []);
+  }, [data]);
 
   const updateImages = () => {
     let images = [];
     data && data.route.images.map((el) => images.push(...el.images));
-    setImages(images);
+    let unique = [...new Set(images.map((el) => el.url))];
+    // console.log("unique", unique);
+    setImages(unique);
   };
+
+  // console.log({ images });
 
   return (
     <Layout>
       <div className="portfolio_container">
-        <h1> {data.name}</h1>
-        <p>Share: </p>
+        <div className="portfolio_info">
+          <h2> {data.route.name}</h2>
+          <p>Share: </p>
+        </div>
         {images &&
           images.map((el) => {
-            return <img src={`${endpoint}${el.url}`} alt="" key={el.id} />;
+            return <img src={`${endpoint}${el}`} alt="" key={el.id} />;
           })}
       </div>
     </Layout>
